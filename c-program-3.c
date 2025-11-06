f32af5c5a92310e963870247e88b9c661d2291bb
harshinibatsala-crypto <harshinibatsala@gmail.com>
2025-11-06 19:19:23 +0530

    Create c-program-3.c
    A SIMPLE PROGRAM TO PRINT SUM AND DIFFERENCE OF THE TWO NUMBERS

---
+++ c-program-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,diff;
+    printf("enter a and b values");
+    scanf(" %d %d",&a,&b);
+    sum=a+b;
+    diff=a-b;
+    printf("sum of digts= %d",sum);
+    printf("\ndiff of digts= %d",diff);
+    return o;
+    
+}
